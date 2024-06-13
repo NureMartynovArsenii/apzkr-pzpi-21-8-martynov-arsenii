7f87c3818b00228fc4092174813e861070be1b8c
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-13 14:30:14 +0300

    Add files via upload

---
+++ apzkr-pzpi-21-8-martynov-arsenii/Task3-WebClient/src/components/FullScreenImage.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import './FullScreenImage.css';
+import sadikImage from './sadik.jpg';
+
+const FullScreenImage = () => {
+    return (
+        <div className="fullscreen-background" style={{ backgroundImage: `url(${sadikImage})` }} />
+    );
+}
+
+export default FullScreenImage;
---
+++ apzkr-pzpi-21-8-martynov-arsenii/Task3-WebClient/src/components/Menu.js
@@ -0,0 +1,150 @@
+﻿import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Menu.css';
+import accountIcon from './account-icon.png';
+
+//const Menu = () => {
+//    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+//    const toggleMenu = () => {
+//        setIsMenuOpen(!isMenuOpen);
+//    };
+
+//    const closeMenu = () => {
+//        setIsMenuOpen(false);
+//    };
+
+//    return (
+//        <div className="menu-container">
+//            <div className="menu-left">
+//                <button className="menu-button">Українська</button>
+//                <button className="menu-button">Англійська</button>
+//            </div>
+//            <div className="menu-center">
+//                ChildClimaCare
+//            </div>
+//            <div className="menu-right">
+//                <img src={accountIcon} className="account-icon" alt="Account Icon" onClick={toggleMenu} />
+//                {isMenuOpen && (
+//                    <div className="dropdown-menu">
+//                        <Link to="/registration" onClick={closeMenu}>Registration</Link>
+//                        <Link to="/authorization" onClick={closeMenu}>Authorization</Link>
+//                        <Link to="/" className="dropdown-item">Main</Link>
+//                        <button onClick={closeMenu}>Close</button>
+//                    </div>
+//                )}
+//            </div>
+//        </div>
+//    );
+//};
+
+//export default Menu;
+
+
+
+//import { useTranslation } from 'react-i18next';
+
+//const Menu = () => {
+//    const [isMenuOpen, setIsMenuOpen] = useState(false);
+//    const { t, i18n } = useTranslation();
+
+//    const toggleMenu = () => {
+//        setIsMenuOpen(!isMenuOpen);
+//    };
+
+//    const closeMenu = () => {
+//        setIsMenuOpen(false);
+//    };
+
+//    const changeLanguage = (lng) => {
+//        i18n.changeLanguage(lng);
+//    };
+
+//    return (
+//        <div className="menu-container">
+//            <div className="menu-left">
+//                <button className="menu-button" onClick={() => changeLanguage('ua')}>{t('Ukrainian')}</button>
+//                <button className="menu-button" onClick={() => changeLanguage('en')}>{t('English')}</button>
+//            </div>
+//            <div className="menu-center">
+//                {t('ChildClimaCare')}
+//            </div>
+//            <div className="menu-right">
+//                <img src={accountIcon} className="account-icon" alt="Account Icon" onClick={toggleMenu} />
+//                {isMenuOpen && (
+//                    <div className="dropdown-menu">
+//                        <Link to="/registration" onClick={closeMenu}>{t('Registration')}</Link>
+//                        <Link to="/authorization" onClick={closeMenu}>{t('Authorization')}</Link>
+//                        <Link to="/" className="dropdown-item">{t('Main')}</Link>
+//                        <button onClick={closeMenu}>Close</button>
+//                    </div>
+//                )}
+//            </div>
+//        </div>
+//    );
+//};
+
+//export default Menu;
+
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+// Вкажіть правильний шлях до іконки
+
+const Menu = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const { t, i18n } = useTranslation();
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    };
+
+    return (
+        <div className="menu-container">
+            <div className="menu-left">
+                <button className="menu-button" onClick={() => changeLanguage('ua')}>{t('Ukrainian')}</button>
+                <button className="menu-button" onClick={() => changeLanguage('en')}>{t('English')}</button>
+            </div>
+            <div className="menu-center">
+                {t('ChildClimaCare')}
+            </div>
+            <div className="menu-right">
+                <img src={accountIcon} className="account-icon" alt="Account Icon" onClick={toggleMenu} />
+                {isMenuOpen && (
+                    <div className="dropdown-menu">
+                        <Link to="/" onClick={closeMenu}>{t('Main')}</Link>
+                        {!token && (
+                            <>
+                                <Link to="/registration" onClick={closeMenu}>{t('Registration')}</Link>
+                                <Link to="/authorization" onClick={closeMenu}>{t('Authorization')}</Link>
+                            </>
+                        )}
+                        {token && (
+                            <>
+                                <Link to="/user/settings" onClick={closeMenu}>{t('My Account')}</Link>
+                                <button onClick={handleLogout}>{t('Logout')}</button>
+                            </>
+                        )}
+                        <button onClick={closeMenu}>Close</button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
