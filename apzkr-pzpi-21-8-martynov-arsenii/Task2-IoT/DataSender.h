51ef384c715f99937c552e75f41a18179384fe3f
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:40:01 +0300

    Update DataSender.h

--- apzkr-pzpi-21-8-martynov-arsenii/Task2-IoT/DataSender.h
+++ apzkr-pzpi-21-8-martynov-arsenii/Task2-IoT/DataSender.h
@@ -1,17 +1,3 @@
-// #ifndef DATASENDER_H
-// #define DATASENDER_H
-
-// #include <ArduinoJson.h>
-
-// class DataSender {
-// public:
-//     void sendSensorData(const char* type, float value);
-//     void startMeasurement(const char* jwtToken);
-//     void stopMeasurement(const char* jwtToken);
-// };
-
-// #endif // DATASENDER_H
-
 #ifndef DATASENDER_H
 #define DATASENDER_H
 
