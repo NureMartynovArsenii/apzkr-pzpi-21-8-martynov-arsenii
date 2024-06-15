3645a1e66bb66cc6103ce73d4bfb01febc6b9fe1
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:34:03 +0300

    Update UserReference.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Models/UserReference.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Models/UserReference.cs
@@ -1,12 +1,6 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
-//public class UserReference
-//{
-//    [BsonRepresentation(BsonType.ObjectId)]
-//    public string Id { get; set; }
-//}
-
 
 public class UserReference
 {
