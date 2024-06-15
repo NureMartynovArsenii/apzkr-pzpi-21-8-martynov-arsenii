ef69b811fe5fe74dbbb1e53d141e7baca59176ef
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:38:41 +0300

    Update RoomService.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Services/RoomService.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Services/RoomService.cs
@@ -11,52 +11,6 @@ public interface IRoomService
 }
 
 
-//public class RoomService : IRoomService
-//{
-//    private readonly IRoomRepository _roomRepository;
-
-//    public RoomService(IRoomRepository roomRepository)
-//    {
-//        _roomRepository = roomRepository;
-//    }
-
-//    public async Task<List<Room>> GetAllAsync()
-//    {
-//        return await _roomRepository.GetAllAsync();
-//    }
-
-//    public async Task<Room> GetByIdAsync(string id)
-//    {
-//        return await _roomRepository.GetByIdAsync(id);
-//    }
-
-//    public async Task<Room> CreateAsync(RoomCreateModel roomCreateModel)
-//    {
-//        var room = new Room
-//        {
-//            RoomNumber = roomCreateModel.RoomNumber,
-//            Capacity = roomCreateModel.Capacity,
-//            GardenId = roomCreateModel.GardenId
-//        };
-//        await _roomRepository.CreateAsync(room);
-//        return room;
-//    }
-
-//    public async Task UpdateAsync(string id, RoomUpdateModel updateModel)
-//    {
-//        var update = Builders<Room>.Update
-//            .Set(r => r.RoomNumber, updateModel.RoomNumber)
-//            .Set(r => r.Capacity, updateModel.Capacity)
-//            .Set(r => r.GardenId, updateModel.GardenId);
-//        await _roomRepository.UpdateAsync(id, update);
-//    }
-
-//    public async Task DeleteAsync(string id)
-//    {
-//        await _roomRepository.DeleteAsync(id);
-//    }
-//}
-
 public class RoomService : IRoomService
 {
     private readonly IRoomRepository _roomRepository;
