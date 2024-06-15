3abd902a2d4676be418b8f6bd206c2f42680e6bf
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:48:20 +0300

    Update Registration.js

--- apzkr-pzpi-21-8-martynov-arsenii/Task3-WebClient/src/pages/user/Registration.js
+++ apzkr-pzpi-21-8-martynov-arsenii/Task3-WebClient/src/pages/user/Registration.js
@@ -1,77 +1,6 @@
 ﻿import React, { useState } from 'react';
 import FullScreenImage from './components/FullScreenImage';
 import './Form.css';
-
-//const Registration = () => {
-//    const [formData, setFormData] = useState({
-//        username: '',
-//        firstName: '',
-//        lastName: '',
-//        email: '',
-//        password: '',
-//        phone: '',
-//        role: '',
-//    });
-
-//    const [message, setMessage] = useState('');
-
-//    const handleChange = (e) => {
-//        const { name, value } = e.target;
-//        setFormData({
-//            ...formData,
-//            [name]: value,
-//        });
-//    };
-
-//    const handleSubmit = async (e) => {
-//        e.preventDefault();
-//        try {
-//            const response = await fetch('https://localhost:7077/api/users/register', {
-//                method: 'POST',
-//                headers: {
-//                    'Content-Type': 'application/json',
-//                },
-//                body: JSON.stringify(formData),
-//            });
-//            if (response.ok) {
-//                setMessage('Успішна реєстрація');
-//            } else {
-//                setMessage('Помилка реєстрації');
-//            }
-//        } catch (error) {
-//            setMessage('Помилка реєстрації');
-//        }
-//    };
-
-//    return (
-//        <div>
-//            <FullScreenImage />
-//            <div className="form-container">
-//                <div className="form-title">Registration</div>
-//                <form className="form" onSubmit={handleSubmit}>
-//                    <input type="text" name="username" placeholder="Username" onChange={handleChange} />
-//                    <input type="text" name="firstName" placeholder="FirstName" onChange={handleChange} />
-//                    <input type="text" name="lastName" placeholder="LastName" onChange={handleChange} />
-//                    <input type="email" name="email" placeholder="Email" onChange={handleChange} />
-//                    <input type="password" name="password" placeholder="Password" onChange={handleChange} />
-//                    <input type="text" name="phone" placeholder="Phone" onChange={handleChange} />
-//                    <select name="role" className="form-select" defaultValue="" onChange={handleChange}>
-//                        <option value="" disabled>Select Role</option>
-//                        <option value="employee">Employee</option>
-//                        <option value="parent">Parent</option>
-//                    </select>
-//                    <button type="submit">Register</button>
-//                </form>
-//                {message && <div className="form-message">{message}</div>}
-//            </div>
-//        </div>
-//    );
-//};
-
-//export default Registration;
-
-
-
 import { useTranslation } from 'react-i18next';
 
 const Registration = () => {
@@ -95,26 +24,7 @@ const Registration = () => {
             [name]: value,
         });
     };
-
-    //const handleSubmit = async (e) => {
-    //    e.preventDefault();
-    //    try {
-    //        const response = await fetch('https://localhost:7077/api/users/register', {
-    //            method: 'POST',
-    //            headers: {
-    //                'Content-Type': 'application/json',
-    //            },
-    //            body: JSON.stringify(formData),
-    //        });
-    //        if (response.ok) {
-    //            setMessage(t('SuccessfulRegistration'));
-    //        } else {
-    //            setMessage(t('RegistrationError'));
-    //        }
-    //    } catch (error) {
-    //        setMessage(t('RegistrationError'));
-    //    }
-    //};
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
