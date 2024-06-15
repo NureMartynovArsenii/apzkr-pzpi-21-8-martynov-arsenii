02d6802ccb19a131f27000cc360cc9c31499acde
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:32:21 +0300

    Update UserMode.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Dtos/UserMode.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Dtos/UserMode.cs
@@ -1,17 +1,17 @@
-﻿//public class CreateUsersModel
-//{
-//    public string Username { get; set; }
-//    public string FirstName { get; set; }
-//    public string LastName { get; set; }
-//    public string Email { get; set; }
-//    public string Password { get; set; }
-//    public string Phone { get; set; }
-//    public string Role { get; set; }
-//    public string Status { get; set; }
-//}
+﻿public class CreateUsersModel
+{
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public string Role { get; set; }
+    public string Status { get; set; }
+}
 
-//public class LoginModel
-//{
-//    public string Username { get; set; }
-//    public string Password { get; set; }
-//}
+public class LoginModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
