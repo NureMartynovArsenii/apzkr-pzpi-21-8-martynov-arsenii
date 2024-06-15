21540bba2ae77240863093fd6e4f33035302883e
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:31:27 +0300

    Update GardenController.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Controllers/GardenController.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Controllers/GardenController.cs
@@ -148,21 +148,6 @@ public class GardensController : ControllerBase
 }
 
 
-
-    public class Garden
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Location { get; set; }
-        public string Director { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public List<UserReference> Users { get; set; }
-    }
-
-
 public class User
 {
     [BsonId]
