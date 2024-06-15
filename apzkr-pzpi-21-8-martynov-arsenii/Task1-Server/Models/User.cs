bdf227a760b35432fae185cb93533e11601d0d80
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:35:11 +0300

    Update User.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Models/User.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Models/User.cs
@@ -1,17 +1,19 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
-//public class Users
-//{
-//    [BsonId]
-//    [BsonRepresentation(BsonType.ObjectId)]
-//    public string Id { get; set; }
-//    public string Username { get; set; }
-//    public string FirstName { get; set; }
-//    public string LastName { get; set; }
-//    public string Email { get; set; }
-//    public string Password { get; set; }
-//    public string Phone { get; set; }
-//    public string Role { get; set; }
-//    public string Status { get; set; }
-//}
+[BsonIgnoreExtraElements]
+public class Users
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    // Пароль не повинен бути відображений у відповіді API
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public string Role { get; set; }
+    public string Status { get; set; }
+}
