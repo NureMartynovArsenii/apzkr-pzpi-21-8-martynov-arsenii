67904b13ed53efb33c45c3a29d12f7987a4b567a
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:32:25 +0300

    Update UsersController.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Controllers/UsersController.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Controllers/UsersController.cs
@@ -450,19 +450,4 @@ public class Users
 }
 
 
-public class LoginModel
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
 
-public class CreateUsersModel
-{
-    public string Username { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string Phone { get; set; }
-    public string Role { get; set; }
-}
