09a6f4a91fcda5e08e6321427fe4fed5c40e127d
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:37:37 +0300

    Update RoomRepository.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Repositories/RoomRepository.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Repositories/RoomRepository.cs
@@ -10,40 +10,6 @@ public interface IRoomRepository
     Task<List<Room>> GetRoomsByGardenIdAsync(string gardenId);
 }
 
-//public class RoomRepository : IRoomRepository
-//{
-//    private readonly IMongoCollection<Room> _collection;
-
-//    public RoomRepository(IMongoDatabase database)
-//    {
-//        _collection = database.GetCollection<Room>("Rooms");
-//    }
-
-//    public async Task<List<Room>> GetAllAsync()
-//    {
-//        return await _collection.Find(_ => true).ToListAsync();
-//    }
-
-//    public async Task<Room> GetByIdAsync(string id)
-//    {
-//        return await _collection.Find(Builders<Room>.Filter.Eq("Id", id)).FirstOrDefaultAsync();
-//    }
-
-//    public async Task CreateAsync(Room room)
-//    {
-//        await _collection.InsertOneAsync(room);
-//    }
-
-//    public async Task UpdateAsync(string id, UpdateDefinition<Room> update)
-//    {
-//        await _collection.UpdateOneAsync(Builders<Room>.Filter.Eq("Id", id), update);
-//    }
-
-//    public async Task DeleteAsync(string id)
-//    {
-//        await _collection.DeleteOneAsync(Builders<Room>.Filter.Eq("Id", id));
-//    }
-//}
 
 public class RoomRepository : IRoomRepository
 {
