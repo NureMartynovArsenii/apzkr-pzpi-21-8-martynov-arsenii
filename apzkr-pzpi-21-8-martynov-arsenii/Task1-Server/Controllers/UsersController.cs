2bc55045a2bad353e00708e75d9cce00dfdab2d4
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:35:29 +0300

    Update UsersController.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Controllers/UsersController.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Controllers/UsersController.cs
@@ -134,22 +134,6 @@ public class UsersController : ControllerBase
     }
 }
 
-[BsonIgnoreExtraElements]
-public class Users
-{
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
-    public string Username { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    // Пароль не повинен бути відображений у відповіді API
-    public string Password { get; set; }
-    public string Phone { get; set; }
-    public string Role { get; set; }
-    public string Status { get; set; }
-}
 
 
 
