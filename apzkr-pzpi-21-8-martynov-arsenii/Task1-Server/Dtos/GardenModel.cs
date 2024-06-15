461911cc2353f2da3ecf74ee355b92bbc819b48e
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:30:42 +0300

    Update GardenModel.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Dtos/GardenModel.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Dtos/GardenModel.cs
@@ -1,20 +1,20 @@
-﻿//public class GardenCreateModel
-//{
-//    public string Name { get; set; }
-//    public string Location { get; set; }
-//    public string Director { get; set; }
-//    public string Email { get; set; }
-//    public string Phone { get; set; }
-//    public List<UserReference> Users { get; set; } = new List<UserReference>();
-//}
+﻿public class GardenCreateModel
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Director { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public List<UserReference> Users { get; set; } = new List<UserReference>();
+}
 
 //// Dtos/GardenUpdateModel.cs
-//public class GardenUpdateModel
-//{
-//    public string Name { get; set; }
-//    public string Location { get; set; }
-//    public string Director { get; set; }
-//    public string Email { get; set; }
-//    public string Phone { get; set; }
-//    public List<UserReference> Users { get; set; } = new List<UserReference>();
-//}
+public class GardenUpdateModel
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Director { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public List<UserReference> Users { get; set; } = new List<UserReference>();
+}
