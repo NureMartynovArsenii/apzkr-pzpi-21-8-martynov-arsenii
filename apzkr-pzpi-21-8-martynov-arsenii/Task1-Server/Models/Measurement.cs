d31a4887da05213795d5eacea1640625af811ed1
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-15 18:33:36 +0300

    Update Measurement.cs

--- apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Models/Measurement.cs
+++ apzkr-pzpi-21-8-martynov-arsenii/Task1-Server/Models/Measurement.cs
@@ -1,15 +1,6 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
-//public class Measurement
-//{
-//    [BsonId]
-//    [BsonRepresentation(BsonType.ObjectId)]
-//    public string Id { get; set; }
-//    public string DeviceId { get; set; }
-//    public string MeasurementType { get; set; }
-//    public string Value { get; set; }
-//}
 
 public class Measurement
 {
