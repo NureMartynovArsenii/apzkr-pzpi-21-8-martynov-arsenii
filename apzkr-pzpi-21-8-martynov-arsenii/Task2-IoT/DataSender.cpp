abac63205627ccca49e5e1c462665ac8f8fe278f
NureMartynovArsenii <162564382+NureMartynovArsenii@users.noreply.github.com>
2024-06-17 19:44:37 +0300

    Update DataSender.cpp

--- apzkr-pzpi-21-8-martynov-arsenii/Task2-IoT/DataSender.cpp
+++ apzkr-pzpi-21-8-martynov-arsenii/Task2-IoT/DataSender.cpp
@@ -2,23 +2,23 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-extern String jwtToken; // Глобальна змінна для токену
+extern String jwtToken; 
 
 void DataSender::sendSensorData(const char* type, float value) {
     if (WiFi.status() == WL_CONNECTED && jwtToken != "") {
         HTTPClient http;
         http.begin("http://2524-2a03-7380-238a-357-906c-3064-56c5-c549.ngrok-free.app/api/measurements");
         http.addHeader("Content-Type", "application/json");
-        http.addHeader("Authorization", "Bearer " + jwtToken); // Додавання заголовку авторизації
+        http.addHeader("Authorization", "Bearer " + jwtToken); 
 
-        StaticJsonDocument<200> jsonDoc; // Увеличен размер буфера
+        StaticJsonDocument<200> jsonDoc; 
         jsonDoc["DeviceId"] = "ESP32_Device";
         jsonDoc["MeasurementType"] = type;
-        jsonDoc["Value"] = String(value); // Преобразование в строку для соответствия типу данных на сервере
+        jsonDoc["Value"] = String(value);
         String requestBody;
         serializeJson(jsonDoc, requestBody);
 
-        Serial.println("Request Body: " + requestBody); // Вывод тела запроса
+        Serial.println("Request Body: " + requestBody); 
 
         int httpResponseCode = http.POST(requestBody);
         if (httpResponseCode > 0) {
@@ -28,7 +28,7 @@ void DataSender::sendSensorData(const char* type, float value) {
         } else {
             Serial.print("Error on sending POST: ");
             Serial.println(httpResponseCode);
-            Serial.println(http.errorToString(httpResponseCode)); // Печать текста ошибки
+            Serial.println(http.errorToString(httpResponseCode)); 
         }
         http.end();
     } else {
